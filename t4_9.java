68a1b018fe955ffddce6c449238f99fbf41898fe
unknown <1308655645@qq.com>
2019-04-28 21:17:32 +0800

    4_9

---
+++ t4_9.java
@@ -0,0 +1,13 @@
+public class t4_9
+{
+	public static void main(String[] args)
+	{
+		
+		String article="pupel";
+		System.out.println(article);
+		article=article.replace("pupil", article);
+		
+ 
+		
+	}
+}
