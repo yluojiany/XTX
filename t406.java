7aab24bdaa615d44e3bbe1d396323f2dfff6ee34
unknown <1308655645@qq.com>
2019-04-28 21:01:35 +0800

    abc

---
+++ t406.java
@@ -0,0 +1,9 @@
+public class t406 { 
+    public static void main(String args[]){ //主方法
+        String num1 = "weifbmioabc";//定义字符串
+        boolean b2 = num1.endsWith("abc");//判断字符串是否以‘abc’结束
+      
+        System.out.println("字符串num1是以'abc'结束的吗？" + b2);
+      
+    }
+}
